0af04bd215c0066109b11c8fb47fb1303a1662c8
Charles <168678941+charlesw-salesforce@users.noreply.github.com>
2026-05-11 14:17:58 -0700

    fix: switch from @testing-library/jest-dom to @testing-library/jest-dom/vitest (#13)

--- force-app/main/react-recipes/uiBundles/reactRecipes/vitest-env.d.ts
+++ force-app/main/react-recipes/uiBundles/reactRecipes/vitest-env.d.ts
@@ -1,12 +1,4 @@
 /// <reference types="vitest" />
-/// <reference types="@testing-library/jest-dom" />
+/// <reference types="@testing-library/jest-dom/vitest" />
 /// <reference types="vitest/globals" />
-
-import type { AxeMatchers } from 'vitest-axe/matchers';
-
-declare module 'vitest' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface Assertion extends AxeMatchers {}
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface AsymmetricMatchersContaining extends AxeMatchers {}
-}
+/// <reference types="vitest-axe/extend-expect" />
